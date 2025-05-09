a2e4e9ca4a238f044033d6e775069c3ea4f4e408
Atheane <eroyant@gmail.com>
2025-05-09 15:42:12 +0200

    fix(spacy): pytorch streamlit compability pb

--- app/pages/5_🔍_Pipeline_filter.py
+++ app/pages/5_🔍_Pipeline_filter.py
@@ -1,5 +1,4 @@
 import markdown
-import pandas as pd
 import spacy
 import streamlit as st
 from bs4 import BeautifulSoup
@@ -18,15 +17,15 @@ def extract_text_markdown(text_markdown):
 @st.cache_resource
 def load_spacy_model():
     try:
-        return spacy.load("fr_dep_news_trf")
+        return spacy.load("fr_core_news_lg")
     except OSError:
         st.error("Installation en cours...")
         import subprocess
 
         subprocess.run(
-            ["python", "-m", "spacy", "download", "fr_dep_news_trf"]
+            ["python", "-m", "spacy", "download", "fr_core_news_lg"]
         )
-        return spacy.load("fr_dep_news_trf")
+        return spacy.load("fr_core_news_lg")
 
 
 st.title("Pipeline de filtrage avec SpaCy")
@@ -43,15 +42,6 @@ if text:
     with st.spinner("Analyse en cours..."):
         text_raw = extract_text_markdown(text)
         doc = nlp(text_raw)
-        # Display named entities
-        st.subheader("Entités nommées")
-        entities = [(ent.text, ent.label_) for ent in doc.ents]
-        df_entities = pd.DataFrame(entities, columns=["Texte", "Type"])
-        st.dataframe(df_entities)
-        # Display syntax analysis
-        st.subheader("Analyse syntaxique")
-        tokens = [(token.text, token.pos_, token.dep_) for token in doc]
-        df_tokens = pd.DataFrame(
-            tokens, columns=["Texte", "PoS", "Dépendance"]
-        )
-        st.dataframe(df_tokens)
+        # Segmenter en phrases
+        for i, sent in enumerate(doc.sents):
+            st.write(f"{sent.text}")
